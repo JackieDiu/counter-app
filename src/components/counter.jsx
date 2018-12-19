1fd680babe3e6ff11d50bcf8b83f5339ec1bba9b
Jackie Diu <jackiediu@gmail.com>
2018-12-19 15:42:12 -0800

    setting up first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
